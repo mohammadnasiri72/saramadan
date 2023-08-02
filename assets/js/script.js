1bbffc7fb1fb3d51e83e16a5fc8f3bf56e0b4844
mohammad <nasiri.mohammadreza92@yahoo.com>
2023-08-02 10:17:33 -0700

    saramadan-part1

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+// const home = document.querySelector(".box-home")
+// home.addEventListener("click" , function(){
+//   const link = document.createElement("a")
+//   // link.target = "_blank"
+//   link.href = "https://google.com"
+//   link.click()
+// })
+
+
+
+const menu = document.querySelector(".icon-menu")
+const menuRespons = document.querySelector(".menu-respons")
+let flagMenu = 0
+menu.addEventListener("click" , function(){
+ if (flagMenu == 0) {
+  menuRespons.style.right = "0"
+  
+  menu.innerHTML = "close"
+  flagMenu = 1
+ }else if (flagMenu == 1) {
+  menuRespons.style.right = "100%"
+  menu.innerHTML = "menu"
+  flagMenu = 0
+ }
+})
